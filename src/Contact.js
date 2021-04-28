8392f57e84314c3f3324e76107153479f4f91c88
yisroelm <yisroel96@gmail.com>
2021-04-28 17:25:44 -0400

    initial commit

---
+++ src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>GOT QUESTIONS?</h2>
+        <p>Click <a href="https://www.iin-business.com/"> here</a> for all the latest in iin content.
+        </p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
---
+++ src/Stuff.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>STUFF</h2>
+        <p>Random Stuff</p>
+        <ol>
+          <li>Options...</li>
+        </ol>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
